8ebabea888ff65ba631122efc59c7c20f1871381
AliNedjar <alinedjarali45@outlook.com>
2020-11-09 14:31:17 +0100

    finished work

--- Speedometer.php
+++ Speedometer.php
@@ -1,8 +1,17 @@
 <?php
 
-static class Speedometer
+class Speedometer
 {
+    private const MILESENKM = 1.609;
+    private const KMENMILES = 0.621;
 
+    public static function changeMilesEnKm($miles)
+    {
+        return $miles * self::MILESENKM;
+    }
 
-
+    public static function changeKmEnMiles($km)
+    {
+        return $km * self::KMENMILES;
+    }
 }
